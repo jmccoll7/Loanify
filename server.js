fdb30738a0a4230bda7a743a3e407c90f1e58b78
James McColl <jmccoll71@gmail.com>
2023-04-22 21:04:58 -0500

    add https

--- server.js
+++ server.js
@@ -1,23 +1,30 @@
-import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import path from "path";
-import bodyParser from "body-parser";
-import cookieParser from "cookie-parser";
-import authenticationRoute from "./routes/authenticationRoute.js";
-import loanRoute from "./routes/loanRoute.js";
-import morgan from "morgan-body";
-import rfs from "rotating-file-stream";
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import path from 'path';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import authenticationRoute from './routes/authenticationRoute.js';
+import loanRoute from './routes/loanRoute.js';
+import morgan from 'morgan-body';
+import rfs from 'rotating-file-stream';
+import fs from 'fs';
+import https from 'https';
 
 dotenv.config();
 
 const app = express();
 
+const options = {
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert'),
+};
+
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
-const accessLogStream = rfs.createStream("access.log", {
-  interval: "1d", // Daily rotation
-  path: path.join(__dirname, "log"),
+const accessLogStream = rfs.createStream('access.log', {
+  interval: '1d', // Daily rotation
+  path: path.join(__dirname, 'log'),
 });
 
 morgan(app, {
@@ -32,19 +39,19 @@ morgan(app, {
 
 app.use(bodyParser.urlencoded({ extended: false }), cookieParser());
 
-app.set("view engine", "ejs");
-app.set("views", "views");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/api", authenticationRoute, loanRoute);
+app.use('/api', authenticationRoute, loanRoute);
 
-app.get("/", (req, res) => {
-  res.redirect("/api/home");
+app.get('/', (req, res) => {
+  res.redirect('/api/home');
 });
 
 app.use((_req, res) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found" });
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 mongoose
@@ -54,7 +61,7 @@ mongoose
   })
   .then(() => {
     const port = process.env.PORT || 3000;
-    app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
+    https.createServer(options, app).listen(port, () => {
+      console.log(`Server running on https://localhost:${port}`);
     });
   });
